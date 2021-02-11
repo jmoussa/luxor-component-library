23d5b988b0703b296f3839f9ae7509f4cfe5cf07
Joseph Moussa <joeph465@gmail.com>
2021-02-10 20:45:46 -0500

    remove comments altogether

--- src/components/stack/Stack.js
+++ src/components/stack/Stack.js
@@ -1,9 +1,5 @@
 import React from 'react'
 import { Box } from '../box/Box'
-//import { Row } from '../row/Row'
-
-// const StackChildrenTypes = [Box, Row]
-// const UnsupportedChildTypeWarning = ('Each child in a Stack component should be one of the types: Box, Rows')
 
 export const Stack = ({ children, space, ...props }) => {
   return (

3903c4654c0f2cb3a7a3485e3b97df2ab252eb21
Joseph Moussa <joeph465@gmail.com>
2021-02-10 20:42:43 -0500

    remove console warning about nesting

--- src/components/row/Row.js
+++ src/components/row/Row.js
@@ -5,12 +5,6 @@ export const Row = ({ children, space, ...props }) => {
   return (
     <Box display='flex' {...props}>
       {React.Children.map(children, (child, index) => {
-        //if (child.type !== Box) {
-        //console.warn(
-        //'Each child in a Rows component should be a Box component'
-        //)
-        //}
-
         if (index > 0) {
           return React.cloneElement(child, {
             marginLeft: space,
--- src/components/stack/Stack.js
+++ src/components/stack/Stack.js
@@ -1,19 +1,14 @@
 import React from 'react'
 import { Box } from '../box/Box'
-import { Row } from '../row/Row'
+//import { Row } from '../row/Row'
 
-const StackChildrenTypes = [Box, Row]
-const UnsupportedChildTypeWarning =
-  'Each child in a Stack component should be one of the types: Box, Rows'
+// const StackChildrenTypes = [Box, Row]
+// const UnsupportedChildTypeWarning = ('Each child in a Stack component should be one of the types: Box, Rows')
 
 export const Stack = ({ children, space, ...props }) => {
   return (
     <Box {...props}>
       {React.Children.map(children, (child, index) => {
-        //if (!StackChildrenTypes.includes(child.type)) {
-        //console.warn(UnsupportedChildTypeWarning)
-        //}
-
         if (index > 0) {
           return React.cloneElement(child, { marginTop: space })
         }
